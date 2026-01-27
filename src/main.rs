f3b090b39c87dc1ea2311bbd37883f86158753b8
Prayash Shakya <prayashshakya20@gmail.com>
2026-01-27 14:09:34 +0545

    feat: finding the directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::path::Path;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2{
+        println!("Usage: organizer <directory>")
+    }
+
+    let dir_path = &args[1];
+    let path = Path::new(dir_path);
+
+    if !path.exists() || !path.is_dir(){
+        println!("{dir_path} is not a valid directory");
+        return;
+    }
+
+    println!("Organizing directory: {dir_path}");
+
 }

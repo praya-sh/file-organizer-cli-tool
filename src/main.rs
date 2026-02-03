ed159a94e1dfb03d81918fb4193828bdceb4f275
Prayash Shakya <prayashshakya20@gmail.com>
2026-02-03 11:53:29 +0545

    map to categories

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::fs;
 
+fn category_for_extentions(ext: &str) ->&str{
+    match ext {
+        "png" | "jpg" | "jpeg" | "gif" => "Images",
+        "pdf" | "doc" | "docx" | "txt" => "Docs",
+        "mp4" | "mkv" | "avi" => "Videos",
+        "mp3" | "wav" => "Audio",
+        "zip" | "rar" | "7z" => "Archives",
+        _ => "Others",
+    }
+}
+
 fn main() {
     let entries = fs::read_dir(".").expect("Failed to read directory");
 
@@ -9,11 +20,11 @@ fn main() {
 
         if path.is_file(){
             let name = path.file_name().unwrap().to_string_lossy();
-            let ext = path.extension().unwrap_or_default().to_string_lossy();
+            let ext = path.extension().unwrap_or_default().to_string_lossy().to_lowercase();
 
-            println!("{name} -> {ext}");
+            let category = category_for_extentions(&ext);
 
-            
+            println!("{name} -> {ext}, {category}");      
         }
     }
 }

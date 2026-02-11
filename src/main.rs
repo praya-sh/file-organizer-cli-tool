ba43f8564584f5b23dc2da7e6e8e5f82c8b87722
Prayash Shakya <prayashshakya20@gmail.com>
2026-02-11 16:05:46 +0545

    code formats, add license, change name

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,33 @@ use std::path::{Path, PathBuf};
 fn category_for_extension(ext: &str) -> &str {
     match ext {
         "png" | "jpg" | "jpeg" | "gif" | "bmp" | "svg" | "webp" => "Images",
-        "pdf" | "doc" | "docx" | "txt" | "rtf"| "otd" => "Docs",
+        "pdf" | "doc" | "docx" | "txt" | "rtf" | "otd" => "Docs",
         "mp4" | "mkv" | "avi" | "mov" | "wmv" | "flv" => "Videos",
-        "mp3" | "wav" | "flac" | "aac" | "ogg"| "m4a" => "Audio",
+        "mp3" | "wav" | "flac" | "aac" | "ogg" | "m4a" => "Audio",
         "zip" | "rar" | "7z" | "tar" | "gz" | "bz2" => "Archives",
         _ => "Others",
     }
 }
 
-fn unique_path(path: PathBuf) -> PathBuf{
-    if !path.exists(){
-        return path
+fn unique_path(path: PathBuf) -> PathBuf {
+    if !path.exists() {
+        return path;
     }
 
     let stem = path.file_stem().unwrap().to_string_lossy();
     let ext = path.extension().map(|e| e.to_string_lossy());
     let parent = path.parent().unwrap();
 
-    for i in 1..{
+    for i in 1.. {
         let mut new_name = format!("{}({})", stem, i);
         if let Some(ext) = &ext {
             new_name.push('.');
             new_name.push_str(ext);
-
         }
 
         let candidate = parent.join(&new_name);
-        if !candidate.exists(){
-            return candidate
+        if !candidate.exists() {
+            return candidate;
         }
     }
 
@@ -69,8 +68,8 @@ fn main() {
 
             let file_name = path.file_name().unwrap();
             let mut new_path = target_dir.join(file_name);
-            
-            if !force{
+
+            if !force {
                 new_path = unique_path(new_path);
             }
 
